3576b6979e40607852ef22ca1c54a2efbf3cc026
SHALfEY088 <shalfey088@gmail.com>
2023-10-09 07:52:58 +0300

    start

---
+++ easy/49. Group Anagrams/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
+	fmt.Println([]string{""})
+	fmt.Println([]string{"a"})
+}
+
+func groupAnagrams(strs []string) [][]string {
+
+}
