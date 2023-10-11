63484015f9e572d281fe460b3475aa66d5a22f65
SHALfEY088 <shalfey088@gmail.com>
2023-10-11 08:34:01 +0300

    solution

--- easy/49. Group Anagrams/main.go
+++ easy/49. Group Anagrams/main.go
@@ -1,14 +1,37 @@
-https://leetcode.com/problems/group-anagrams/description/
+// https://leetcode.com/problems/group-anagrams/description/
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println([]string{"eat", "tea", "tan", "ate", "nat", "bat"})
-	fmt.Println([]string{""})
-	fmt.Println([]string{"a"})
+	//groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}) //["bat"],["nat","tan"],["ate","eat","tea"]]
+	fmt.Println(groupAnagrams([]string{"eat", "tea", "tan", "ate", "nat", "bat"}))
+	fmt.Println(groupAnagrams([]string{""}))
+	fmt.Println(groupAnagrams([]string{"a"}))
 }
 
 func groupAnagrams(strs []string) [][]string {
-	return make([][]string, 0)
+	anMap := make(map[[26]int][]string)
+	for _, s := range strs {
+
+		var cnt [26]int
+
+		for _, c := range s {
+			cnt[c-'a']++
+		}
+
+		anMap[cnt] = append(anMap[cnt], s)
+		//fmt.Printf("%v\n", anMap)
+
+	}
+
+	var res = make([][]string, len(anMap))
+	idx := 0
+	for _, v := range anMap {
+		res[idx] = v
+		idx++
+	}
+
+	//fmt.Println(res)
+	return res
 }
