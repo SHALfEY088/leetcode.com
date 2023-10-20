c484ecd7929bf2a45b1be97f23b1d33d5c25e14c
SHALfEY088 <shalfey088@gmail.com>
2023-10-20 03:28:41 +0300

    added 347

---
+++ medium/347. Top K Frequent Elements/main.go
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/top-k-frequent-elements/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(topKFrequent([]int{1, 1, 1, 2, 2, 3}, 2))
+	fmt.Println(topKFrequent([]int{1}, 1))
+}
+
+func topKFrequent(nums []int, k int) []int {
+
+	return make([]int, 0)
+}
