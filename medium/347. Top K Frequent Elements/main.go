e5512d38a86c34bb18c5b6ff3ddd90429b4c21bd
SHALfEY088 <shalfey088@gmail.com>
2023-10-26 05:43:09 +0300

    solution

--- medium/347. Top K Frequent Elements/main.go
+++ medium/347. Top K Frequent Elements/main.go
@@ -4,11 +4,35 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(topKFrequent([]int{1, 1, 1, 2, 2, 3}, 2))
-	fmt.Println(topKFrequent([]int{1}, 1))
+	fmt.Println(topKFrequent([]int{1, 1, 1, 2, 2, 3}, 2)) // [1, 2]
+	//fmt.Println(topKFrequent([]int{1}, 1))                // [1]
+	fmt.Println(topKFrequent([]int{1, 2}, 2)) // [1, 2]
 }
 
-func topKFrequent(nums []int, k int) []int {
+func topKFrequent(nums []int, k int) (res []int) {
+	cntMp := make(map[int]int)
 
-	return make([]int, 0)
+	for _, num := range nums {
+		if cnt, exist := cntMp[num]; exist {
+			cntMp[num] = cnt + 1
+		} else {
+			cntMp[num] = 1
+		}
+	}
+
+	cntSl := make([][]int, len(nums)+1)
+
+	for num, cnt := range cntMp {
+		cntSl[cnt] = append(cntSl[cnt], num)
+	}
+
+	for i := len(cntSl) - 1; i > 0; i-- {
+		res = append(res, cntSl[i]...)
+		if len(res) == k {
+			return
+		}
+
+	}
+
+	return
 }
